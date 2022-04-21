a1130726a4657ed57c5367158a1c9d5ae75c3df1
raemundo <eimooamai@outlook.com>
2022-04-21 14:23:28 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import SideNav from "./components/SideNav";
+import './styles/main.css';
+import './assets/Your-Doodle-Font.ttf';
+import './assets/Baloo-Regular.ttf';
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <SideNav />
+        <main className="main">
+          <div className="btn-container">
+            <button className="connect-btn">Conncect Wallet</button>
+          </div>
+          <div className="msg-container">
+            <div>
+              <h1 className="head">
+                the bloodeverse<br />
+                welcome to <br />
+              </h1>
+              <h2 className="desc">
+                connect your wallet to enter the app
+              </h2>
+            </div>
+          </div>
+
+        </main>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/SideNav.tsx
@@ -0,0 +1,89 @@
+import React from "react";
+import Work from "./imgs/Work.png"
+import BLMBankIcon from "./imgs/BLMBankIcon.png"
+import breeding from "./imgs/breeding.png"
+import DashboardIcon from "./imgs/DashboardIcon.png"
+import EduCertIcon from "./imgs/EduCertIcon.png"
+import HousingIcon from "./imgs/HousingIcon.png"
+import MarketplaceIcon from "./imgs/MarketplaceIcon.png"
+import twitter from "./imgs/twitter.png"
+import discord from "./imgs/discord.png"
+import Bloodle_Male from "./imgs/Bloodle_Male.png"
+
+const SideNav = () => {
+  return (
+    <aside className="sidebar" data-sidebar>
+      <div className="top-sidebar">
+        <a href="#" className="logo">
+          <img src={Bloodle_Male} />
+        </a>
+      </div>
+      <div className="middle-sidebar">
+        <ul className="sidebar-list">
+          <li className="sidebar-list-item">
+            <a href="#" className="sidebar-link">
+              <img src={DashboardIcon} />
+              <div className="sidebar-txt">Text Here</div>
+            </a>
+          </li>
+          <li className="sidebar-list-item">
+            <a href="#" className="sidebar-link">
+              <img src={Work} />
+              <div className="sidebar-txt">Text Here</div>
+            </a>
+          </li>
+          <li className="sidebar-list-item">
+            <a href="#" className="sidebar-link">
+              <img src={breeding} />
+              <div className="sidebar-txt">Text Here</div>
+            </a>
+          </li>
+
+          
+          <li className="sidebar-list-item">
+            <a href="#" className="sidebar-link">
+              <img src={HousingIcon} />
+              <div className="sidebar-txt">Text Here</div>
+            </a>
+          </li>
+          <li className="sidebar-list-item">
+            <a href="#" className="sidebar-link">
+              <img src={EduCertIcon} />
+              <div className="sidebar-txt">Text Here</div>
+            </a>
+          </li>
+          <li className="sidebar-list-item">
+            <a href="#" className="sidebar-link">
+              <img src={MarketplaceIcon} />
+              <div className="sidebar-txt">Text Here</div>
+            </a>
+          </li>
+        
+          <li className="sidebar-list-item">
+            <a href="#" className="sidebar-link">
+              <img src={BLMBankIcon} />
+              <div className="sidebar-txt">Text Here</div>
+            </a>
+          </li>
+
+        </ul>
+      </div>
+      <div className="bottom-sidebar">
+        <ul className="sidebar-list">
+          <li className="sidebar-list-item">
+            <a href="#" className="sidebar-link">
+              <img src={twitter} />
+            </a>
+          </li>
+          <li className="sidebar-list-item">
+            <a href="#" className="sidebar-link">
+              <img src={discord} />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </aside>
+  )
+};
+
+export default SideNav;
