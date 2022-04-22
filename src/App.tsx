188b77c9837eb154f2d0544447ba15e32095d945
raemundo <eimooamai@outlook.com>
2022-04-22 11:22:33 +0200

    fix: resposive, pixel-perfex design

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,19 @@
 import React from "react";
-import SideNav from "./components/SideNav";
+import Sidebar from "./components/Sidebar";
 import './styles/main.css';
 import './assets/Your-Doodle-Font.ttf';
 import './assets/Baloo-Regular.ttf';
 import CardContainer from "./components/CardContainer/indxe";
+import Head from "./components/Head";
 function App() {
   return (
     <>
-      <div className="container">
-        <SideNav />
+      <div className="d-flex w-full">
+        <Sidebar />
         <main className="main">
-          <div className="flex space-between header-c">
-
-            <div className="first-column">
-              <div className="main-txt">Send your bloodles to work</div>
-              <button className="connect-btn">Exit the Bloodeverse</button>
-
-            </div>
-
-            <div className="">
-              <div className="">
-                <button className="connect-btn">0xBE4256....Xsd</button>
-              </div>
-              <div className="">
-                <p>Balance: <span >1500$</span> <span>BLM</span></p>
-              </div>
-            </div>
-          </div>
-
-
-
+          <Head/>
           <CardContainer num={4} headTxt="bloodles working" />
           <CardContainer num={5} headTxt="bloodles on cooldown" />
-
         </main>
       </div>
     </>
