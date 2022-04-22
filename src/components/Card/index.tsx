dc6ecc769c787a7586b71a1cbf4862009d4ae32c
raemundo <eimooamai@outlook.com>
2022-04-22 11:21:29 +0200

    fix: card

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import person from "./person.png"
-export default function Card () {
+import "./Card.css";
+export default function Card() {
     return (
-        <div className="card">
-            <img src={person} />
+        <div className="card-st">
+            <img src={person} alt="" />
             <div className="cid">#1234</div>
-            <div className="duration">2h remaining hours</div>
+            <div className="duration">2h remaining</div>
         </div>
     )
 }
