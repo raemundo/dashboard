ddf5f55b93e83b0892c092fdbac78b4982e315eb
raemundo <eimooamai@outlook.com>
2022-04-22 11:21:49 +0200

    fix: card-container

--- src/components/CardContainer/indxe.tsx
+++ src/components/CardContainer/indxe.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Card from '../Card'
+import "./CardContainer.css";
 
 export default function CardContainer({num, headTxt}) {
     return (
-        <div className="card-container">
-
+        <div className='card-container'>
             <h2 className="head">{headTxt}</h2>
-            <div className="flex">
+            <div className="d-flex flex-wrap">
                 {/* @ts-ignore */}
                 {[...Array(num).keys()].map((idx, item) => (
                     <Card key={idx} />
